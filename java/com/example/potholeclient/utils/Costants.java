15422f527e4c94be2c6abd6c90b22edfb2830f80
MarianoCaccavale <75137536+MarianoCaccavale@users.noreply.github.com>
2022-05-09 10:51:33 +0200

    Update Costants.java

--- java/com/example/potholeclient/utils/Costants.java
+++ java/com/example/potholeclient/utils/Costants.java
@@ -2,8 +2,8 @@ package com.example.potholeclient.utils;
 
 public class Costants {
 
-    public static String ip = "20.71.131.210";
-    public static int port = 9876;
+    public static String ip = "*insert your ip here*";
+    public static int port = 4242;
 
     public static String nickname = "";
     public static double toleranceThreshold = 1;
